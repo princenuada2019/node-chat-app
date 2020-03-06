79a205f594accfaf2d4244ef268847a65c6d4724
princenuada2019 <43705327+princenuada2019@users.noreply.github.com>
2020-03-06 13:05:39 +0330

    Init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+var publicPath = path.join(__dirname, '../public');
+
+/*console.log(__dirname + '/../public');
+console.log(publicPath);*/
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});
